9e14e8ae0e773f7d7c4425f0d0658a5e77de9e26
Kaylee <kayleenasser@gmail.com>
2024-09-20 15:15:35 -0600

    Added more images

--- input.py
+++ input.py
@@ -9,15 +9,10 @@ def editdata():
     print('edit kaylees data')
     while True:
         item = input('Enter item: ')
-        if item =='':
+        if item == '':
             command = input('Close data editor? (Y/N): ')
             if (command.lower() == 'y' or command.lower()=='yes'):
                 break
-        elif item not in book:
-            for each in stats:
-                answer = input('Enter '+each+': ')
-                entry.update({each:answer})
-                book.update({item:entry})
         elif item in book:
             print('Entry found: ', item)
             for each in book[item]:
@@ -26,11 +21,17 @@ def editdata():
             if(request.lower() == 'n' or request.lower() == 'no'):
                 continue
             else:
-                for each in stats:
-                    answer = input('Enter '+each+': ')
-                    entry.update({each:answer})
+                for stat in stats:
+                    answer = input('Enter '+ stat +': ')
+                    entry.update({stat:answer})
                 book.update({item:entry})
                 continue
+        elif item not in book:
+            for stat in stats:
+                answer = input('Enter '+ stat +': ')
+                entry.update({stat:answer})
+            book.update({item:entry})
+            print({item:entry})
 
 action = ''
 print('welcome kaylee data inputter!')

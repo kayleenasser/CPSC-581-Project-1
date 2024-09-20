0d30cb45e0f0aea18a0cdf3812701e6d25d4f20c
Kaylee <kayleenasser@gmail.com>
2024-09-20 16:07:03 -0600

    Fixed json thingy

--- input.py
+++ input.py
@@ -5,41 +5,27 @@ book = {}
 entry = {}
 stats = ['fact','opinion','percent','image']
 
-def editdata():
-    print('edit kaylees data')
-    while True:
-        item = input('Enter item: ')
-        if item == '':
-            command = input('Close data editor? (Y/N): ')
-            if (command.lower() == 'y' or command.lower()=='yes'):
-                break
-        elif item in book:
-            print('Entry found: ', item)
-            for each in book[item]:
-                print(each, ':', book[item][each])
-            request = input('edit? (y/n) ')
-            if(request.lower() == 'n' or request.lower() == 'no'):
-                continue
-            else:
-                for stat in stats:
-                    answer = input('Enter '+ stat +': ')
-                    entry.update({stat:answer})
-                book.update({item:entry})
-                continue
-        elif item not in book:
-            for stat in stats:
-                answer = input('Enter '+ stat +': ')
-                entry.update({stat:answer})
-            book.update({item:entry})
-            print({item:entry})
-
-action = ''
 print('welcome kaylee data inputter!')
-with open(path,"r") as f:
-    book = f.read()
-book = json.loads(book)
+while True:
+    book = {}
+    item = input('Enter item: ')
+    if item == '':
+        command = input('Close data editor? (Y/N): ')
+        if (command.lower() == 'y' or command.lower()=='yes'):
+            break
+    for stat in stats:
+        answer = input('Enter '+ stat +': ')
+        if stat == 'image':
+            answer = r"../assets/" +answer+'.png'
+        entry.update({stat:answer})
+    book.update({item:entry})
+    with open(path, 'r') as infile:
+        try:
+            old = json.load(infile)
+        except:
+            continue
+        book.update(old)
+    
+    with open(path,"w") as f:
+        json.dump(book,f)
 
-editdata()
-
-with open(path,"w") as f:
-    json.dump(book,f)
